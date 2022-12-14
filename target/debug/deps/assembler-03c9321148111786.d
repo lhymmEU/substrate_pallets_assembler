82f2ffb9e58cc83e334779ba29e3e16516ebd05b
lhymmEU <88905788+lhymmEU@users.noreply.github.com>
2022-12-15 00:29:47 +0100

    Delete target directory

--- target/debug/deps/assembler-03c9321148111786.d
+++
@@ -1,5 +0,0 @@
-/Users/lhymm/rust/substrate_pallets_assembler/target/debug/deps/assembler-03c9321148111786: assembler/src/main.rs
-
-/Users/lhymm/rust/substrate_pallets_assembler/target/debug/deps/assembler-03c9321148111786.d: assembler/src/main.rs
-
-assembler/src/main.rs:
--- target/debug/libserver.d
+++
@@ -1,1 +0,0 @@
-/Users/lhymm/rust/substrate_pallets_assembler/target/debug/libserver.rlib: /Users/lhymm/rust/substrate_pallets_assembler/server/src/lib.rs /Users/lhymm/rust/substrate_pallets_assembler/server/src/routes/assemble.rs /Users/lhymm/rust/substrate_pallets_assembler/server/src/routes/mod.rs /Users/lhymm/rust/substrate_pallets_assembler/server/src/start.rs
